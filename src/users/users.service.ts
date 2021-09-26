419e3b79fa356ae3f353e79a320f61e23a0b38e6
Junyoung Lee <ambitiouskyle@gmail.com>
2021-09-26 18:16:49 +0900

    MODIFY: Welcome Coupon issuance DELETE: Success interceptor

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,28 +1,51 @@
+import { UserCoupons } from './../entities/user-coupons.entity';
 import {
   BadRequestException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../entities/user.entity';
-import { Repository } from 'typeorm';
+import { Repository, Connection } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+
+import { User } from '../entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
+    private connection: Connection,
   ) {}
 
-  async createUser(data: CreateUserDto) {
-    const checkUser = await this.userRepository.findOne({ email: data.email });
+  async createUser(
+    data: CreateUserDto,
+  ): Promise<{ success: boolean; error?: string }> {
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    const checkUser = await queryRunner.manager
+      .getRepository(User)
+      .findOne({ email: data.email });
     if (checkUser) {
       throw new BadRequestException('ACCOUNT_ALREADY_EXISTS');
     }
-
     data.password = await bcrypt.hash(data.password, 10);
-    await this.userRepository.save(data);
+
+    try {
+      const newUser = await queryRunner.manager.getRepository(User).save(data);
+      await queryRunner.manager.getRepository(UserCoupons).save({
+        userId: newUser.id,
+        couponId: 3,
+      });
+      await queryRunner.commitTransaction();
+      return { success: true };
+    } catch {
+      await queryRunner.rollbackTransaction();
+      return { success: false, error: 'ACCOUNT_ALREADY_EXISTS' };
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async findUserById(id: number): Promise<User | null> {
